3725ef2723af50b678efdea1eeefecb0511a7ba3
Harshit Gaur <harshit.gaur6155@gmail.com>
2022-03-06 00:14:11 -0500

    Total Votes, altogether and segregated by Parties; Total Unemployment Rate -- 3 Line Table Descriptive Statistics

--- Proposal_EDA_RScript.R
+++ Proposal_EDA_RScript.R
@@ -17,14 +17,60 @@ ElectionData <- read.csv("~/Documents/Northeastern University/MPS Analytics/ALY 
 # Get a Glimpse/View of the data set
 glimpse(ElectionData)
 
+
 # Descriptive Statistics of 'Total Votes' in 2008, 2012, 2016
-totalVotes <- ElectionData %>% 
+totalVotesStats <- ElectionData %>% 
   select(v2008, v2012, v2016)
+# Stargazer Method
+stargazer(totalVotesStats, type = "text", title = "Table 1: Descriptive Statistics for Total Votes", 
+          out = "total_votes_table.txt", iqr = TRUE, median = TRUE, style = "jpam")
+# Write Table Method
+write.table(totalVotesStats, file = "Documents/Northeastern University/MPS Analytics/ALY 6015/Final Project/Presidential_Election_2016_Classification/total_votes_table_doc.doc", sep = ",")
+
+# Kable Classic Method
+totalVotesStats <-  totalVotesStats %>% 
+  describe(quant = c(.25, .75), IQR = TRUE) %>% 
+  mutate(year = c(2008, 2012, 2016)) %>% 
+  relocate(year)
+round(totalVotesStats, 2) %>% 
+  kbl(caption = "Table 1: Descriptive Statistics for Total Votes") %>% 
+  kable_classic(html_font = "Cambria")
 
 
-View(describe(totalVotes))
-stargazer(totalVotes, type = "text", title = "Table 1: Total Votes", out = "total_votes_table.txt")
+# Descriptive Statistics of 'Total Democratic Votes' in 2008, 2012, 2016
+totalDemocraticVotesStats <- ElectionData %>% 
+  select(vd2008, vd2012, vd2016) %>% 
+  describe(quant = c(.25, .75), IQR = TRUE) %>% 
+  mutate(year = c(2008, 2012, 2016)) %>% 
+  relocate(year)
+# Kable Classic Method
+round(totalDemocraticVotesStats, 2) %>% 
+  kbl(caption = "Table 2: Descriptive Statistics for Total Democratic Votes") %>% 
+  kable_classic(html_font = "Cambria")
 
+
+# Descriptive Statistics of 'Total Republican Votes' in 2008, 2012, 2016
+totalRepublicanVotes <- ElectionData %>% 
+  select(vg2008, vg2012, vg2016) %>% 
+  describe(quant = c(.25, .75), IQR = TRUE) %>% 
+  mutate(year = c(2008, 2012, 2016)) %>% 
+  relocate(year)
+# Kable Classic Method
+round(totalRepublicanVotes, 2) %>% 
+  kbl(caption = "Table 3: Descriptive Statistics for Total Republican Votes") %>% 
+  kable_classic(html_font = "Cambria")
+
+
+# Descriptive Statistics of 'Total Unemployment Rate' in 2011, 2012, 2013, 2014, 2015
+unemploymentRate <- ElectionData %>% 
+  select(unemp11, unemp12, unemp13, unemp14, unemp15) %>% 
+  describe(quant = c(.25, .75), IQR = TRUE) %>% 
+  mutate(year = c(2011, 2012, 2013, 2014, 2015)) %>% 
+  relocate(year)
+# Kable Classic Method
+round(unemploymentRate, 2) %>% 
+  kbl(caption = "Table 4: Descriptive Statistics for Total Unemployment Rate") %>% 
+  kable_classic(html_font = "Cambria")
 
 
 
